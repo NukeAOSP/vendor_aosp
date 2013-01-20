e9110bf2aa29405e91d1dfdd997c996f137f0d8e
NukeAOSP <fly03043090@gmail.com>
2013-01-20 13:46:06 +0200

    Update config/version.mk

--- config/version.mk
+++ config/version.mk
@@ -1,15 +1,7 @@
-## XYAOSP Version prop ##
+## Nuke Version prop ##
 DATE = $(shell date -u +%Y%m%d)
 
 # For System Information & Build zip name
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.xylon.version=$(TARGET_PRODUCT)-official-016-1 \
-    ro.modversion=$(TARGET_PRODUCT)_016-1_$(DATE)
-
-# For XYStats
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.xylon.isitrom=Xylon \
-    ro.xylon.modname=Xylon \
-    ro.xylon.modversion=Official_016-1 \
-    ro.xylon.device=$(TARGET_PRODUCT) \
-    ro.xylon.builtdate=$(DATE)
+    ro.nuke.version=$(TARGET_PRODUCT)-1.4 \
+    ro.modversion=$(TARGET_PRODUCT)_1.4_$(DATE)
